2bd6a2619b0a2bc3c66cb753332cd6387bc357f7
Ian-Woo Kim <ianwookim@gmail.com>
2022-06-17 13:13:42 -0700

    start GUI app

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import qualified GI.Gtk as Gtk
+import qualified GI.Gtk.Enums as Gtk
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  Gtk.init Nothing
+  window <- Gtk.windowNew Gtk.WindowTypeToplevel
+  Gtk.onWidgetDestroy window Gtk.mainQuit
+  Gtk.setWindowTitle window "Hello World"
+  Gtk.widgetShowAll window
+  Gtk.main
--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
     flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        hsenv = pkgs.haskellPackages.ghcWithPackages (p: [ p.cabal-install ]);
+        hsenv = pkgs.haskellPackages.ghcWithPackages
+          (p: [ p.cabal-install p.gi-gtk ]);
       in { devShell = pkgs.mkShell { buildInputs = [ hsenv ]; }; });
 }
