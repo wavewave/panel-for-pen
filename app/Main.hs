94acbaa41222ab47f423fe5e4df08f9319892b5e
Ian-Woo Kim <ianwookim@gmail.com>
2022-06-17 13:39:46 -0700

    create a window always on top and at the top left corner.

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,26 @@
 
 module Main where
 
-import GI.Gtk (AttrOp ((:=)), new, on)
+import qualified GI.Gdk.Enums as Gdk
+import GI.Gtk (AttrOp ((:=)), new, on, set)
 import qualified GI.Gtk as Gtk
 import qualified GI.Gtk.Enums as Gtk
 
 main :: IO ()
 main = do
   Gtk.init Nothing
-  window <- new Gtk.Window [#title := "Hello World"]
+  window <-
+    new
+      Gtk.Window
+      [ #title := "panel-for-pen",
+        #gravity := Gdk.GravityNorthWest,
+        #decorated := False
+      ]
   on window #destroy Gtk.mainQuit
+  button <- new Gtk.Button [#label := "Close"]
+  on button #clicked Gtk.mainQuit
+  set window [#child := button]
+  #setKeepAbove window True
+  #move window 0 0
   #showAll window
   Gtk.main
