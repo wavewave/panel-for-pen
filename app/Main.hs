016a0b356c92110e69d7f3bfb8e6925fae1c45e4
Ian-Woo Kim <ianwookim@gmail.com>
2022-06-17 15:39:07 -0700

    screen-sized window width

--- app/Main.hs
+++ app/Main.hs
@@ -3,26 +3,38 @@
 
 module Main where
 
+import qualified GI.Gdk as Gdk
 import qualified GI.Gdk.Enums as Gdk
-import GI.Gtk (AttrOp ((:=)), new, on, set)
+import GI.Gtk (AttrOp ((:=)), get, new, on, set)
 import qualified GI.Gtk as Gtk
 import qualified GI.Gtk.Enums as Gtk
 
 main :: IO ()
 main = do
   Gtk.init Nothing
-  window <-
-    new
-      Gtk.Window
-      [ #title := "panel-for-pen",
-        #gravity := Gdk.GravityNorthWest,
-        #decorated := False
-      ]
-  on window #destroy Gtk.mainQuit
-  button <- new Gtk.Button [#label := "Close"]
-  on button #clicked Gtk.mainQuit
-  set window [#child := button]
-  #setKeepAbove window True
-  #move window 0 0
-  #showAll window
-  Gtk.main
+  mdisplay <- Gdk.displayGetDefault
+  case mdisplay of
+    Nothing -> error "no display?"
+    Just display -> do
+      mmonitor <- #getPrimaryMonitor display
+      case mmonitor of
+        Nothing -> error "no monitor?"
+        Just monitor -> do
+          rect <- #getGeometry monitor
+          width <- get rect #width
+          window <-
+            new
+              Gtk.Window
+              [ #title := "panel-for-pen",
+                #gravity := Gdk.GravityNorthWest,
+                #decorated := False
+              ]
+          Gtk.widgetSetSizeRequest window width 30
+          on window #destroy Gtk.mainQuit
+          button <- new Gtk.Button [#label := "Close"]
+          on button #clicked Gtk.mainQuit
+          set window [#child := button]
+          #setKeepAbove window True
+          #move window 0 0
+          #showAll window
+          Gtk.main
