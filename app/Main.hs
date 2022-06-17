99aac66294f086985685a19ae578c8f16b8e3b06
Ian-Woo Kim <ianwookim@gmail.com>
2022-06-17 13:19:47 -0700

    use overloading style

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
+{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
+import GI.Gtk (AttrOp ((:=)), new, on)
 import qualified GI.Gtk as Gtk
 import qualified GI.Gtk.Enums as Gtk
 
 main :: IO ()
 main = do
   Gtk.init Nothing
-  window <- Gtk.windowNew Gtk.WindowTypeToplevel
-  Gtk.onWidgetDestroy window Gtk.mainQuit
-  Gtk.setWindowTitle window "Hello World"
-  Gtk.widgetShowAll window
+  window <- new Gtk.Window [#title := "Hello World"]
+  on window #destroy Gtk.mainQuit
+  #showAll window
   Gtk.main
